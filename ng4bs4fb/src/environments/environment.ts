afb204928e7e2b814c6a9e653dde3a00b6e679da
Joao <jv.goulart.almeida@hotmail.com>
2017-12-20 18:40:35 -0200

    adicionado firebase

--- ng4bs4fb/src/environments/environment.ts
+++ ng4bs4fb/src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAeoZyBx2cpLYOLgoKANrDUg16ha0Nb-7o",
+    authDomain: "ionictreino.firebaseapp.com",
+    databaseURL: "https://ionictreino.firebaseio.com",
+    projectId: "ionictreino",
+    storageBucket: "ionictreino.appspot.com",
+    messagingSenderId: "884622598983"
+  }
 };
