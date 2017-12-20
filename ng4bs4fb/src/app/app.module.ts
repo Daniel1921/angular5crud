309074121d4f164e810331da2ff024dfea4602e1
Joao <jv.goulart.almeida@hotmail.com>
2017-12-20 20:32:57 -0200

    ajustes

--- ng4bs4fb/src/app/app.module.ts
+++ ng4bs4fb/src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AppComponent } from './app.component';
-
+import { environment } from '../environments/environment.prod';
+import { AppNavbarComponent } from './app-navbar/app-navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppNavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
+ 
 })
-export class AppModule { }
+export class AppModule {  }
