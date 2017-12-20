3267d5762d5ad137b1f7b8192309e02e251d0d2a
Joao <jv.goulart.almeida@hotmail.com>
2017-12-20 21:40:07 -0200

    crud funcional

--- ng4bs4fb/src/app/app.module.ts
+++ ng4bs4fb/src/app/app.module.ts
@@ -6,17 +6,27 @@ import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment.prod';
 import { AppNavbarComponent } from './app-navbar/app-navbar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CursosListaComponent } from './cursos-lista/cursos-lista.component';
+import { FormComponent } from './form/form.component';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppNavbarComponent
+    AppNavbarComponent,
+    CursosListaComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    NgbModule.forRoot()
 
   ],
   providers: [],
